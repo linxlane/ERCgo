45bd062bf3ba1edf9da95cba9818f1d730bc06a0
Linnea Lane <linnealane@gmail.com>
2025-01-19 20:15:07 -0800

    change generateLookupDict() name to camel case rather than underscores

--- ERCgo/__main__.py
+++ ERCgo/__main__.py
@@ -1,18 +1,17 @@
 import cli
 import hog_comp_ids
-import pandas
 
 #Parse user input from the command line
 args = cli.runParser()
 #Generate dictionary of flags parsed in cli.py
 argsDict = vars(args)
 
-#print(argsDict)
 edgeFilePath = hog_comp_ids.findEdgeFile(argsDict['input'])
 verticesFilePath = hog_comp_ids.findVerticesFile(argsDict['input'])
 
-hogCompDict = hog_comp_ids.generate_lookup_dict(verticesFilePath)
+hogCompDict = hog_comp_ids.generateLookupDict(verticesFilePath)
 
 geneLookupDF_FULL = hog_comp_ids.generateHogCompTable(edgeFilePath, hogCompDict)
 
 geneLookupDF_DROP = hog_comp_ids.dropNaRows(geneLookupDF_FULL)
+
--- ERCgo/hog_comp_ids.py
+++ ERCgo/hog_comp_ids.py
@@ -25,7 +25,7 @@ def findVerticesFile(directory):
   else:
     print('Too many vertices files found.')
 
-def generate_lookup_dict(path):
+def generateLookupDict(path):
   df = pandas.read_csv(path, sep='\t', usecols=['HOG_ID', 'Comprehensive_ID'])
   hog_dict = df.set_index('HOG_ID')['Comprehensive_ID'].to_dict()
   return hog_dict
