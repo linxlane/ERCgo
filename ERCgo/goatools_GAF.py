f5e19f80ae35d1c1ad2d25d979521e651e36eba4
Linnea Lane <linnealane@gmail.com>
2025-01-19 20:34:53 -0800

    generate gene id and list of GO terms and write to file

--- ERCgo/goatools_GAF.py
+++ ERCgo/goatools_GAF.py
@@ -1,13 +1,21 @@
 from goatools.anno.gaf_reader import GafReader
+import pandas
 
 def readGAF(gafPath):
   ogaf = GafReader(gafPath)
 
   ns2assc = ogaf.get_ns2assc()
 
+  geneAndGoList = []
+
   for namespace, associations in ns2assc.items():
-      for protein_id, go_ids in sorted(associations.items())[:100]:
-          print("{NS} {PROT:7} : {GOs}".format(
-              NS=namespace,
-              PROT=protein_id,
-              GOs=' '.join(sorted(go_ids))))
+    for protein_id, go_ids in sorted(associations.items()):
+      geneGoPair = []
+      geneGoPair.append(protein_id)
+      geneGoPair.append(sorted(go_ids))
+      geneAndGoList.append(geneGoPair)
+
+  geneAndGoListdf = pandas.DataFrame(geneAndGoList, columns=['Gene ID', 'GO Terms'])
+  geneAndGoListdf.to_csv('ID_GO_TERMS_TABLE.tsv', sep='\t', index=False)
+
+          
