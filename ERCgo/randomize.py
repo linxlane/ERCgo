3e13b588b347d001cc2cb60568e8b726dc613db6
Linnea Lane <linnealane@gmail.com>
2025-01-26 17:00:00 -0800

    remove print statements

--- ERCgo/randomize.py
+++ ERCgo/randomize.py
@@ -15,14 +15,11 @@ def randomDirectory(outDirectory):
 
 def generateRandomizedFiles(edgeFile, randomDirectory, reps):
     GeneA_HOG_col = pandas.read_csv(edgeFile, sep='\t', usecols=['GeneA_HOG'])
-    print(GeneA_HOG_col)
     GeneB_HOG_col = pandas.read_csv(edgeFile, sep='\t', usecols=['GeneB_HOG'])
-    print(GeneB_HOG_col)
 
     for i in range(1, reps + 1):
       print("Randomizing ERCnet edge network data and writing in directory")
       rand_GeneB_HOG_col = GeneB_HOG_col.sample(frac=1).reset_index(drop=True)
-      print(rand_GeneB_HOG_col)    
       hogARandHogB = pandas.concat([GeneA_HOG_col, rand_GeneB_HOG_col], axis=1)
       newFilePath = randomDirectory + '/' + 'network_edges_' + 'rand_' + str(i) + '.tsv'
       hogARandHogB.to_csv(newFilePath, sep='\t', index=False)
