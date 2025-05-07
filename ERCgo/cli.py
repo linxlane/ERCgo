02dcf7994f6667da20be84af9119131cf3e4e122
Linnea Lane <linnealane@gmail.com>
2025-05-06 17:57:07 -0700

    update cli arguments: add -j and remove randomization args

--- ERCgo/cli.py
+++ ERCgo/cli.py
@@ -3,16 +3,16 @@ import argparse
 def runParser():
   parser = argparse.ArgumentParser()
 
+  parser.add_argument('-j', '--job_name', required=True,
+    help='''Job name for this run of ERCgo. If a directory with this job name already exists at the output path, it will be erased and rewritten.
+            Avoid including spaces or special characters ("_" is ok)''')
+
   parser.add_argument('-i', '--input', required=True, metavar='dir_path',
     help='''Path to ERCnet output files and GAF file which will be used in the GO analysis''')
   
-  parser.add_argument('-o', '--output', required=True, metavar='dir_path',
-    help='''Path to output folder where ERCnet GO analysis information will be written''')
-  
-  parser.add_argument('-r', '--random', required=True, type = int, metavar='int',
-    help='''Number of random replicates to generate and analyze.''')
-  
-  parser.add_argument('-m', '--rand_method', required=False, default='B', choices=['AB', 'B'],
-    help='''Randomize both A and B columns or just B column in randomized edge files''')
+  parser.add_argument('-o', '--output', required=False, metavar='dir_path',
+    help='''Path where new directory for ERCgo output will be created with the job name. 
+            If not included, it will be written in the ERCgo directory.
+            ''')
   
   return parser.parse_args()
