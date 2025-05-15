dd89542421fbdfaea97c5b95559fc0a84123a804
Linnea Lane <linnealane@gmail.com>
2025-05-14 21:47:18 -0700

    commit changes for clp branch

--- stats_plots.py
+++ stats_plots.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib as mpl
-from scipy.stats import mannwhitneyu, rankdata, pearsonr
+from scipy.stats import mannwhitneyu, rankdata, pearsonr, spearmanr, linregress
 
 def checkOutputDirectory(outPath):
   if not os.path.exists(outPath):
@@ -27,9 +27,15 @@ def checkOutputDirectory(outPath):
 
 def pearsonCorrelation(ercData):
   # Calculate Pearson correlation and p-value
-  pearson_corr, pearson_pval = pearsonr(ercData['Overlap_Score'], ercData['P_Pval'])
+  pearson_corr, pearson_pval = pearsonr(ercData['Overlap_Score'], ercData['P_R2'])
   print('pearson_corr: ' + str(pearson_corr))
   print('pearson_pval: ' + str(pearson_pval))
+
+def spearmanCorrelation(ercData):
+  # Calculate Pearson correlation and p-value
+  spearman_corr, spearman_pval = spearmanr(ercData['Overlap_Score'], ercData['P_R2'])
+  print('spearman_corr: ' + str(spearman_corr))
+  print('spearman_pval: ' + str(spearman_pval))
 
 def negLog10(col):
   values = -np.log10(col)
@@ -43,7 +49,7 @@ def scatterPlot(ercData):
   replaceZerosDF = ercData.replace(to_replace=0, value=smallValue)
   #print(replaceZerosDF.head(50))
 
-  replaceZerosDF['negLog10'] = negLog10(replaceZerosDF['P_Pval'])
+  #replaceZerosDF['negLog10'] = negLog10(replaceZerosDF['P_Pval'])
 
   alphaAlphaMask = replaceZerosDF['Color'] == 'Alpha-Alpha'
   betaBetaMask = replaceZerosDF['Color'] == 'Beta-Beta'
@@ -60,43 +66,74 @@ def scatterPlot(ercData):
 
 
 
-  allPoints = sns.scatterplot(data=replaceZerosDF[noInterestMask], x='Overlap_Score', y='negLog10', marker='X', color='green', zorder=1)
-  rprRprPlot = sns.scatterplot(data=replaceZerosDF[rptRptMask], x='Overlap_Score', y='negLog10', marker='o', color='yellow',  zorder=2)
-  rpnRpnPlot = sns.scatterplot(data=replaceZerosDF[rpnRpnMask], x='Overlap_Score', y='negLog10', marker='o', color='purple',  zorder=2)
-  betaBetaPlot = sns.scatterplot(data=replaceZerosDF[betaBetaMask], x='Overlap_Score', y='negLog10', marker='o', color='orange',  zorder=2)
-  alphaAlphaPlot = sns.scatterplot(data=replaceZerosDF[alphaAlphaMask], x='Overlap_Score', y='negLog10', marker='o', color='blue', zorder=2)
-  otherPlot = sns.scatterplot(data=replaceZerosDF[otherMask], x='Overlap_Score', y='negLog10', marker='o', color='red', zorder=2)
+  allPoints = sns.scatterplot(data=replaceZerosDF[noInterestMask], x='Overlap_Score', y='P_R2', marker='X', color='green', zorder=1)
+  rprRprPlot = sns.scatterplot(data=replaceZerosDF[rptRptMask], x='Overlap_Score', y='P_R2', marker='o', color='yellow',  zorder=2)
+  rpnRpnPlot = sns.scatterplot(data=replaceZerosDF[rpnRpnMask], x='Overlap_Score', y='P_R2', marker='o', color='purple',  zorder=2)
+  betaBetaPlot = sns.scatterplot(data=replaceZerosDF[betaBetaMask], x='Overlap_Score', y='P_R2', marker='o', color='orange',  zorder=2)
+  alphaAlphaPlot = sns.scatterplot(data=replaceZerosDF[alphaAlphaMask], x='Overlap_Score', y='P_R2', marker='o', color='blue', zorder=2)
+  otherPlot = sns.scatterplot(data=replaceZerosDF[otherMask], x='Overlap_Score', y='P_R2', marker='o', color='red', zorder=2)
 
   '''
   for point in range(len(rpnRpnDF)):
     label = getAGI(rpnRpnDF['COMP_GENE_A'][point], agiDict) + '-' + getAGI(rpnRpnDF['COMP_GENE_B'][point], agiDict)
-    plt.text(x=rpnRpnDF['Overlap_Score'][point], y=rpnRpnDF['negLog10'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='purple')
+    plt.text(x=rpnRpnDF['Overlap_Score'][point], y=rpnRpnDF['P_R2'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='purple')
 
   for point in range(len(rptRptDF)):
     label = getAGI(rptRptDF['COMP_GENE_A'][point], agiDict) + '-' + getAGI(rptRptDF['COMP_GENE_B'][point], agiDict)
-    plt.text(x=rptRptDF['Overlap_Score'][point], y=rptRptDF['negLog10'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='purple')
+    plt.text(x=rptRptDF['Overlap_Score'][point], y=rptRptDF['P_R2'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='purple')
 
 
   for point in range(len(betaBetaDF)):
     label = getAGI(betaBetaDF['COMP_GENE_A'][point], agiDict) + '-' + getAGI(betaBetaDF['COMP_GENE_B'][point], agiDict)
-    plt.text(x=betaBetaDF['Overlap_Score'][point], y=betaBetaDF['negLog10'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='orange')
+    plt.text(x=betaBetaDF['Overlap_Score'][point], y=betaBetaDF['P_R2'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='orange')
   
   for point in range(len(alphaAlphaDF)):
     label = getAGI(alphaAlphaDF['COMP_GENE_A'][point], agiDict) + '-' + getAGI(alphaAlphaDF['COMP_GENE_B'][point], agiDict)
-    plt.text(x=alphaAlphaDF['Overlap_Score'][point], y=alphaAlphaDF['negLog10'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='blue')
+    plt.text(x=alphaAlphaDF['Overlap_Score'][point], y=alphaAlphaDF['P_R2'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='blue')
   
   for point in range(len(otherDF)):
     label = getAGI(otherDF['COMP_GENE_A'][point], agiDict) + '-' + getAGI(otherDF['COMP_GENE_B'][point], agiDict)
-    plt.text(x=otherDF['Overlap_Score'][point], y=otherDF['negLog10'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='blue')
+    plt.text(x=otherDF['Overlap_Score'][point], y=otherDF['P_R2'][point], s=label, horizontalalignment='center', verticalalignment='bottom', color='blue')
   '''
 
   plt.xscale('log')
   
-  plt.ylabel('-log10(P_Pval)')
+  plt.ylabel('P_R2')
 
-  plt.title('BXB')
+  plt.title('Proteosome BXB Hits')
 
   plt.show()
+
+def filterHits(ercData):
+  hitsDf = ercData[ercData['P_Pval'] < 0.0001] 
+  hitsDf = hitsDf[hitsDf['P_R2'] > 0.4]
+  hitsDf = hitsDf[hitsDf['slope'] > 0.0]
+  nonHitsDf = ercData[ercData['P_Pval'] > 0.0001] 
+  nonHitsDf = nonHitsDf[nonHitsDf['P_R2'] < 0.4]
+  hitsDf = hitsDf[hitsDf['slope'] < 0.0]
+
+  return hitsDf, nonHitsDf
+
+def kde(hitData, nonHitData):
+  fig, ax = plt.subplots(figsize=(12, 6))
+  hitsSmallValue = hitData['Overlap_Score'][hitData['Overlap_Score'] != 0].min()
+  hitsReplaceZerosDF = hitData.replace(to_replace=0, value=hitsSmallValue)
+  nonHitsSmallValue = nonHitData['Overlap_Score'][nonHitData['Overlap_Score'] != 0].min()
+  nonHitsReplaceZerosDF = hitData.replace(to_replace=0, value=nonHitsSmallValue)
+
+  sns.kdeplot(data=hitsReplaceZerosDF['Overlap_Score'], color='orange', label='hits', ax=ax)
+  sns.kdeplot(data=nonHitsReplaceZerosDF['Overlap_Score'], color='blue', label='non-hits', ax=ax)
+  ax.legend()
+  plt.xscale('log')
+  #plt.tight_layout()
+  plt.show()
+
+def mannwhitney(hits, nonhits):
+  # Perform the one-sided Mann-Whitney U test (sample1 > sample2)
+  stat, p = mannwhitneyu(hits, nonhits, alternative='greater')
+
+  print(f"Statistic: {stat}")
+  print(f"P-value: {p}")
 
 ##Start of main stat and plotting script
 print('################################################')
@@ -124,12 +161,40 @@ except:
 print('---------------------------------------------------------------------------------------------------')
 print('Correlation Statistics')
 print('---------------------------------------------------------------------------------------------------')
+slope = linregress(goAnalysisDf['Overlap_Score'], goAnalysisDf['P_R2']).slope
+print(f"linregress slope: {slope}")
+
 pearsonCorrelation(goAnalysisDf)
+spearmanCorrelation(goAnalysisDf)
 
 print('---------------------------------------------------------------------------------------------------')
 print('Scatterplot')
 print('---------------------------------------------------------------------------------------------------')
-scatterPlot(goAnalysisDf)
+#scatterPlot(goAnalysisDf)
+print('Skip')
+
+print('---------------------------------------------------------------------------------------------------')
+print('KDE')
+print('---------------------------------------------------------------------------------------------------')
+hits, nonHits = filterHits(goAnalysisDf)
+print('Hit Rows')
+print(len(hits))
+print('Non-Hit Rows')
+print(len(nonHits))
+print('Hit Max')
+print(hits['Overlap_Score'].max())
+print('Non-Hit Max')
+print(nonHits['Overlap_Score'].max())
+print('Hit Value Counts')
+print(hits['Overlap_Score'].value_counts())
+print('Non-Hit Value Counts')
+print(nonHits['Overlap_Score'].value_counts())
+
+#print(hits.head())
+#print(nonHits.head())
+print('Mannwhitneyu test')
+mannwhitney(hits['Overlap_Score'], nonHits['Overlap_Score'])
+kde(hits, nonHits)
 
 print('\n')
 print('###########################################')
