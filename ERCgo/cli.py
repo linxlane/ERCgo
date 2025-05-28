8d135b345e31cb7bbc83790ff07435d0a3988a7d
Linnea Lane <linnealane@gmail.com>
2025-05-28 15:20:53 -0700

    update cli with directory input option

--- ERCgo/cli.py
+++ ERCgo/cli.py
@@ -7,15 +7,18 @@ def runParser():
     help='''Job name for this run of ERCgo. If a directory with this job name already exists at the output path, it will be erased and rewritten.
             Avoid including spaces or special characters ("_" is ok)''')
 
-  parser.add_argument('-i', '--interactome', required=True, metavar='dir_path',
-    help='''Path to interactome that will be used in the GO analysis''')
+  parser.add_argument('-i', '--interactome', required=False, metavar='path',
+    help='''Path to interactome that will be used for the GO analysis''')
   
-  parser.add_argument('-g', '--gaf', required=True, metavar='dir_path',
-    help='''Path to gene association file that will be used in the GO analysis''')
+  parser.add_argument('-g', '--gaf', required=False, metavar='path',
+    help='''Path to gene association file that will be used for the GO analysis''')
+  
+  parser.add_argument('-d', '--directory_input', required=False, metavar='dir_path',
+    help='''Path to directory which contains the interactome and gene association files that will be used in the GO analysis''')
   
   parser.add_argument('-o', '--output', required=False, metavar='dir_path', default=None,
     help='''Path where new directory for ERCgo output will be created with the job name. 
-            If not included, it will be written in the ERCgo directory.
+            If not included, it will be written in the ERCgo OUTPUT directory.
             If this path already exists, it will be deleted and a new directory will be created at the output_directory/job_name path.
             ''')
   
